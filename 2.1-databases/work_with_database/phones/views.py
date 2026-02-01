d84e2f7c851040477ac8e862fce668463d7207b4
Artem Lobachyov <lobachyov.art@yandex.ru>
2026-02-01 16:53:10 +0300

    Task completed

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,32 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
 
 
 def show_catalog(request):
+    sorting = request.GET.get('sort')
+    phones = Phone.objects.all()
+    if sorting == 'name':
+        phones = sorted(phones, key=lambda x: x.name)
+    elif sorting == 'min_price':
+        phones = sorted(phones, key=lambda x: x.price)
+    elif sorting == 'max_price':
+        phones = sorted(phones, key=lambda x: x.price, reverse=True)
+
     template = 'catalog.html'
-    context = {}
+    context = {
+        'phones': phones
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    context = {
+        'phone': Phone.objects.filter(slug=slug)[0]
+    }
     return render(request, template, context)
